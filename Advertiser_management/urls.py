33c4df913bc7069f92a5f26d1d2662f0a65dda79
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2021-01-23 12:04:25 +0330

    when you click ad is ready

--- Advertiser_management/urls.py
+++ Advertiser_management/urls.py
@@ -3,5 +3,6 @@ from django.urls import  path
 from . import views
 
 urlpatterns = [
-    path('',views.show_ad,name = 'ads'),
+    path('/',views.show_ad,name = 'ads'),
+    path('create/',views.make_ad,name='creation'),
 ]
--- Advertiser_management/views.py
+++ Advertiser_management/views.py
@@ -2,13 +2,32 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Ad,Advertiser
 from django.shortcuts import render
-
+from django.forms import Form
+from django.http import HttpResponseRedirect,Http404,HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import  loader
 # Create your views here.
 def show_ad(request):
-    return render(request,'../templates/Advertiser_management/ads.html',{})
+    listOfAds = Ad.objects.order_by('advertiser_id')
+    return render(request, 'Advertiser_management/ads.html', {
+        'Ads': listOfAds,
+    })
 
-def guide_user_after_clcick(request):
-    pass
+def guide_user_after_click(request,ad_id):
+   ad = get_object_or_404(Ad,pk = ad_id)
+   ad.incClicks()
+   ad.incViews()
+   ad.save()
+   return HttpResponseRedirect(ad.get_link())
 
 def make_ad(request):
-    return render(request,'../templates/Advertiser_management/makingAd.html',{})
+    return render(request, 'Advertiser_management/makingAd.html', {})
+
+
+def handleCreationAd(request):
+    title = request.POST['title']
+    print(title)
