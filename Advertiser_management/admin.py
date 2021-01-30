f5452256baf98aa8f02c961a280f91be994c279a
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2021-01-30 13:26:28 +0330

    filter and search for ad in admin.py

--- Advertiser_management/admin.py
+++ Advertiser_management/admin.py
@@ -6,6 +6,8 @@ class AdAdmin(admin.ModelAdmin):
      fields = ('approve',('title','link','image','advertiser',))
      readonly_fields =  ('title','link','image','advertiser',)
      list_display = ('title','advertiser','approve')
+     list_filter = ('approve',)
+     search_fields = ['title__exact',]
 
 admin.site.register(Ad,AdAdmin)
 
