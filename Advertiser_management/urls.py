269116ef99c1498693fd3cc5433664d130fbe143
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2021-01-31 17:13:34 +0330

    view class based are replaced defs

--- Advertiser_management/urls.py
+++ Advertiser_management/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('show/',views.ShowAdPage.as_view(),name = 'ads'),
-    path('create/',views.make_ad,name='creation'),
-    path('create/getPost/',views.make_ad,name = 'creation'),
-    path('click/<int:ad_id>/',views.guide_user_after_click,name = 'ads'),
+    path('create/',views.MAkingAdPage.as_view(),name='creation'),
+    path('create/getPost/',views.MAkingAdPage.as_view(),name = 'creation'),
+    path('click/<int:ad_id>/',views.GuideAfterClick.as_view(),name = 'ads link page'),
 ]
--- Advertiser_management/views.py
+++ Advertiser_management/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404
-from django.shortcuts import render
 from django.utils import timezone
-from ipware import get_client_ip
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic.edit import FormView
+
 from .forms import data_form
 from .models import Ad, Advertiser
-from django.views.generic.base import TemplateView
-
 
 
 # Create your views here.
@@ -22,39 +21,38 @@ class ShowAdPage(TemplateView):
         context['advertisers'] = list_advertisers
         return context
 
-
-def guide_user_after_click(request,ad_id):
-        ad = get_object_or_404(Ad,pk = ad_id)
-        user_id_str = request.META.get('HTTP_X_FORWARDED_FOR')
+class GuideAfterClick(RedirectView):
+    permanent = False
+    pattern_name = 'ads link page'
+    def get_redirect_url(self, *args, **kwargs):
+        ad = get_object_or_404(Ad, pk=kwargs['ad_id'])
+        user_id_str = self.request.META.get('HTTP_X_FORWARDED_FOR')
         ip = ''
         if user_id_str:
             ip = user_id_str.split(',')[0]
         else:
-            ip = request.META.get('REMOTE_ADDR')
-        ad.inc_clicks(time=timezone.now(),user=ip)
-        ad.inc_views(time=timezone.now(),user=ip)
+            ip = self.request.META.get('REMOTE_ADDR')
+        ad.inc_clicks(time=timezone.now(), user=ip)
+        ad.inc_views(time=timezone.now(), user=ip)
         ad.save()
-        return HttpResponseRedirect(ad.get_link())
-
-
-
-
-def make_ad(request):
-    if request.method == 'POST':
-        form = data_form(request.POST)
-        if form.is_valid():
-          try:
+        return ad.get_link()
+class MAkingAdPage(FormView):
+    template_name = 'Advertiser_management/makingAd.html'
+    form_class = data_form
+    success_url = 'getPost/'
+    def form_valid(self, form):
+        try:
             advertiser_find = form.cleaned_data.get('advertiser')
             advertiser = Advertiser.objects.get(advertiser_id=advertiser_find)
-            ad = Ad(title=form.cleaned_data.get('title') , link=form.cleaned_data.get('link') , image= form.cleaned_data.get('image') , advertiser= advertiser)
+            ad = Ad(title=form.cleaned_data.get('title'), link=form.cleaned_data.get('link'),
+                    image=form.cleaned_data.get('image'), advertiser=advertiser)
             ad.save()
             return HttpResponseRedirect('/ads/show/')
-          except:
-              raise Http404('the error is in finding advertiser')
+        except:
+            raise Http404('the error is in finding advertiser')
 
-    else:
-        form = data_form()
-    return render(request, 'Advertiser_management/makingAd.html', {'form':form})
+
+
 
 
 
